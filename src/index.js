2fd88b73a2a4bd339453b7acd6657fd3eb3c6052
Selmi11895480 <selmi.kerim@gmail.com>
2018-07-23 14:30:52 +0100

    rn dotenv fixed with babel :hammer:

---
+++ src/index.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import {
+    ActivityIndicator,
+    AsyncStorage,
+    Button,
+    StatusBar,
+    StyleSheet,
+    View,
+} from 'react-native';
+
+
+import Login from './Login'
+class AuthLoadingScreen extends React.Component {
+    constructor() {
+        super();
+        this._bootstrapAsync();
+    }
+
+    
+
+    // Fetch the token from storage then navigate to our appropriate place
+    _bootstrapAsync = async () => {
+        const userToken = await AsyncStorage.getItem('acces_token');
+
+        // This will switch to the App screen or Auth screen and this loading
+        // screen will be unmounted and thrown away.
+        this.props.navigation.navigate(userToken ? 'App' : 'Auth');
+    };
+
+    // Render any loading content that you like here
+    render() {
+        return (
+            <View style={styles.container}>
+                <ActivityIndicator />
+                <StatusBar barStyle="default" />
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
