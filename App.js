252ccf60d76b24d0393b6a428c9f01418261f37f
Selmi11895480 <selmi.kerim@gmail.com>
2018-07-23 17:47:27 +0100

    entry point

--- App.js
+++ App.js
@@ -6,6 +6,12 @@ export default class HelloWorldApp extends Component {
   render() {
     return (
       <SteemConnect />
+      /**
+       *  style={{ width: 48, height: 48 }}
+          size={GoogleSigninButton.Size.Icon}
+          color={GoogleSigninButton.Color.Dark}
+          onPress={this._signIn}
+       */
     );
   }
 }
