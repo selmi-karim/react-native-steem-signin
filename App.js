080183df0cbb5fb0eada7c2b3107f52be630e8d4
Selmi11895480 <selmi.kerim@gmail.com>
2018-07-23 16:59:14 +0100

    entry point

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
 import SteemConnect from './src/index'
+
 export default class HelloWorldApp extends Component {
   render() {
     return (
